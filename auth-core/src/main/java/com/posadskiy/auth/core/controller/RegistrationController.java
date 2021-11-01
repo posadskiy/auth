f31dbeed5204ad0ccff302a538b4c0dfe4d516ef
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2021-11-01 09:10:54 +0300

    Sessions implementation for user registration

--- auth-core/src/main/java/com/posadskiy/auth/core/controller/RegistrationController.java
+++ auth-core/src/main/java/com/posadskiy/auth/core/controller/RegistrationController.java
@@ -2,6 +2,8 @@ package com.posadskiy.auth.core.controller;
 
 import com.posadskiy.auth.api.dto.User;
 
+import javax.servlet.http.HttpServletResponse;
+
 public interface RegistrationController {
-	User registration(final User user);
+	User registration(final User user, final String sessionId, final HttpServletResponse response);
 }
--- auth-web/src/main/java/com/posadskiy/auth/web/endpoint/RegistrationEndpoint.java
+++ auth-web/src/main/java/com/posadskiy/auth/web/endpoint/RegistrationEndpoint.java
@@ -1,9 +1,30 @@
 package com.posadskiy.auth.web.endpoint;
 
+import com.posadskiy.auth.api.dto.User;
+import com.posadskiy.auth.core.controller.RegistrationController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("reg")
 public class RegistrationEndpoint {
+	final RegistrationController registrationController;
+
+	@Autowired
+	public RegistrationEndpoint(RegistrationController registrationController) {
+		this.registrationController = registrationController;
+	}
+
+	@PostMapping("v1/registration")
+	public User registration(@RequestBody final User user,
+					 final HttpServletRequest request,
+					 final HttpServletResponse response) {
+		return registrationController.registration(user, request.getSession().getId(), response);
+	}
 }
